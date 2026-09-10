374ef6757b28af2bdf65a65a435211570cc3c05a
vundalegowripriya-svg <vundalegowripriya@gmail.com>
2026-09-10 22:13:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,12 @@ def create_database():
 
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS daily_attendance (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            try:
+    conn.execute("ALTER TABLE daily_attendance ADD COLUMN status TEXT DEFAULT 'Present'")
+    conn.commit()
+except sqlite3.OperationalError:
+    pass
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
             student_id TEXT NOT NULL,
             attendance_date TEXT NOT NULL,
             start_time TEXT NOT NULL,
